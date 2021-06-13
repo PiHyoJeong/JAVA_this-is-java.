cd6c5814203372f0cc274c63e260d800d87fc15d
PiHyoJeong <78262975+PiHyoJeong@users.noreply.github.com>
2021-06-13 13:56:00 +0900

    chap07_sec08_exam02_abstract_method

---
+++ exam02_abstract_method/AnimalExample.java
@@ -0,0 +1,29 @@
+package sec08.exam02_abstract_method;
+
+public class AnimalExample {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		
+		dog.sound();
+		cat.sound();
+		System.out.println("------------------");
+		
+		Animal animal = null;
+		animal = new Dog(); //자동 타입 변환
+		animal.sound();
+		animal = new Cat(); //자동 타입 변환
+		animal.sound();
+		
+		System.out.println("------------------");
+		animalSound(new Dog());
+		animalSound(new Cat());
+
+	}
+	
+	public static void animalSound(Animal animal) {
+		animal.sound(); 
+	}
+
+}
