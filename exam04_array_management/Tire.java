4e67546a80a48d187e9d8491b6e91bb4b53cf8fa
PiHyoJeong <78262975+PiHyoJeong@users.noreply.github.com>
2021-06-13 13:54:45 +0900

    chap07_sec07_exam04_array_management

---
+++ exam04_array_management/Tire.java
@@ -0,0 +1,24 @@
+package sec07.exam04_array_management;
+
+public class Tire {
+	public int maxRotation; //최대회전수
+	public int accumulatedRotation; //누적회전수
+	public String location;
+	
+	public Tire(String location,int maxRotation) {
+		this.maxRotation = maxRotation;
+		this.location = location;
+	}
+	
+	public boolean roll() {
+		++accumulatedRotation;
+		if(maxRotation > accumulatedRotation) {
+			System.out.println(location + "타이어의 남은 수명 : " + (maxRotation-accumulatedRotation));
+			return true;
+		}else {
+			 System.out.println(location + "타이어가 펑크가 났습니다.");
+			return false;
+		}
+	}
+	
+}
