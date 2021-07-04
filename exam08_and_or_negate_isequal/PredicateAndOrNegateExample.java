927fe7357708dfdd962df293b6a4a37c32be4951
PiHyoJeong <78262975+PiHyoJeong@users.noreply.github.com>
2021-07-04 23:20:19 +0900

    chap14_sec05_exam08_and_or_negate_isequal

---
+++ exam08_and_or_negate_isequal/PredicateAndOrNegateExample.java
@@ -0,0 +1,36 @@
+package sec05.exam08_and_or_negate_isequal;
+
+import java.util.function.IntPredicate;
+
+public class PredicateAndOrNegateExample {
+
+	public static void main(String[] args) {
+		//2의 배수 검사
+		IntPredicate predicateA = a -> a%2==0;
+		
+		//3의 배수 검사 
+		IntPredicate predicateB = a -> a%3==0;
+		
+		IntPredicate predicateAB;
+		boolean result;
+		
+		//and()
+		predicateAB = predicateA.and(predicateB);
+		result = predicateAB.test(9);
+		System.out.println("9는 2와 3의 배수입니까? " + result);
+		
+		//or()
+		predicateAB = predicateA.or(predicateB);
+		result = predicateAB.test(9);
+		System.out.println("9는 2의 배수이거나 3의 배수입니까? " + result);
+		
+		//negate()
+		predicateAB = predicateA.negate();
+		result = predicateAB.test(9);
+		System.out.println("9는 2와 3의 배수가 아닙니까?" + result);
+
+	}
+
+}
+
+
